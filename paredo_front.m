6e640fc90375b3c1d8447e9e992a480e9581303d
Bryce Ingersoll <bryceingersoll@byu.edu>
2016-01-30 10:25:24 -0700

    paredo_front change
    Added another paredo front change.

--- paredo_front.m
+++ paredo_front.m
@@ -9,13 +9,18 @@ close all;
 
 %rng(9), 50/4/3
 % ms = 3
-time = [29.4 81.8 175.6 375.8 516.2 778.43];
-dis = [ 160.0276
-  147.2014
-  146.5553
-  146.2599
-  146.2529
-  146.2064 ];
+% time = [29.4 81.8 175.6 375.8 516.2 778.43];
+% dis = [ 160.0276
+%   147.2014
+%   146.5553
+%   146.2599
+%   146.2529
+%   146.2064 ];
+
+%rng(3), 50/4/3
+% ms = 3;
+time = [45 93 127 204 344 596];
+dis = [172.253923641734;170.410239996284;149.849643976224;148.351190385225;148.053963065152;147.600466512524];
 
 figure(1);
 hold on;
@@ -29,7 +34,7 @@ legend('1 segment planned','2 segments planned','3 segments planned','4 segments
        '5 segments planned','6 segments planned');
 xlabel('Algorithm Completion Time (s)');
 ylabel('Total Distance (m)');
-xlim([0 800]);
-ylim([145 165]);
+xlim([0 650]);
+ylim([145 175]);
 title('Number of Planned Path Segment Comparison');
 hold off;
